eacccc7ce65be75ffd343d57d37f992b7ec55be2
Aakash Pothepalli <p.saiaakash518@gmail.com>
2022-06-24 21:17:07 +0530

    exposed execute api

--- lib/cpp/index.ts
+++ lib/cpp/index.ts
@@ -1,9 +1,11 @@
 import { runCppFile } from './run-file';
 import { runCppSource } from './run-source';
+import { execute } from "../execute-command";
 
 const cpp = {
     runFile: runCppFile,
-    runSource: runCppSource
+    runSource: runCppSource,
+    execute:execute
 };
 
 export default cpp;
